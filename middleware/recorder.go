05cb1677c26dcfa0d05dadf45e7d5f9688330730
michaelact <86778470+michaelact@users.noreply.github.com>
2022-06-24 12:40:46 +0700

    style: convert spaces into tabs

--- middleware/recorder.go
+++ middleware/recorder.go
@@ -1,13 +1,13 @@
 package middleware
 
 import (
-    "net/http"
-    "strconv"
+	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/rs/zerolog"
 
-    "github.com/michaelact/firstApi/model/log"
+	"github.com/michaelact/firstApi/model/log"
 )
 
 type RecorderMiddleware struct {
@@ -24,34 +24,34 @@ func NewRecorderMiddleware(handler http.Handler, logger *zerolog.Logger) *Record
 
 func (self *RecorderMiddleware) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	start := time.Now()
-    logField := &log.HTTP{
-        ReceivedTime:      start,
-        RequestMethod:     req.Method,
-        RequestURL:        req.URL.String(),
-        UserAgent:         req.UserAgent(),
-        Referer:           req.Referer(),
-        Proto:             req.Proto,
-        RemoteIP:          req.RemoteAddr,
-    }
+	logField := &log.HTTP{
+		ReceivedTime:      start,
+		RequestMethod:     req.Method,
+		RequestURL:        req.URL.String(),
+		UserAgent:         req.UserAgent(),
+		Referer:           req.Referer(),
+		Proto:             req.Proto,
+		RemoteIP:          req.RemoteAddr,
+	}
 
-    self.Handler.ServeHTTP(res, req)
+	self.Handler.ServeHTTP(res, req)
 
-    statusCode, err := strconv.Atoi(res.Header().Get("StatusCode"))
-    if err != nil {
-        self.Logger.Warn().Err(err)
-    }
+	statusCode, err := strconv.Atoi(res.Header().Get("StatusCode"))
+	if err != nil {
+		self.Logger.Warn().Err(err)
+	}
 
-    logField.StatusCode = statusCode
-    logField.Latency = time.Since(start)
-    self.Logger.Info().
-	    Time("received_time", logField.ReceivedTime).
-        Str("method", logField.RequestMethod).
-        Str("url", logField.RequestURL).
-        Str("agent", logField.UserAgent).
-        Str("referer", logField.Referer).
-        Str("proto", logField.Proto).
-        Str("remote_ip", logField.RemoteIP).
-        Int("status", logField.StatusCode).
-        Dur("latency", logField.Latency).
-        Msg("")
+	logField.StatusCode = statusCode
+	logField.Latency = time.Since(start)
+	self.Logger.Info().
+		Time("received_time", logField.ReceivedTime).
+		Str("method", logField.RequestMethod).
+		Str("url", logField.RequestURL).
+		Str("agent", logField.UserAgent).
+		Str("referer", logField.Referer).
+		Str("proto", logField.Proto).
+		Str("remote_ip", logField.RemoteIP).
+		Int("status", logField.StatusCode).
+		Dur("latency", logField.Latency).
+		Msg("")
 }
